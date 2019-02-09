8517e12708025f483861f519ed049825514a6044
Illarion Koperski <illarion@outlook.com>
2019-02-09 13:28:26 +0100

    Add loadBicycleAssets action to world

--- src/machines/wizard/world.js
+++ src/machines/wizard/world.js
@@ -1,7 +1,13 @@
 import axios from 'axios'
+import { forEach } from 'lodash/fp'
 
 import { store } from '../../ducks'
+
 import { bicycle } from '../../ducks/bicycle'
+import { drivetrain } from '../../ducks/drivetrain'
+import { paint } from '../../ducks/paint'
+import { pedal } from '../../ducks/pedal'
+import { saddle } from '../../ducks/saddle'
 
 const { PUBLIC_URL = '' } = process.env
 
@@ -12,5 +18,28 @@ export const world = {
     })
 
     store.dispatch(bicycle.add(data))
+  },
+  loadBicycleAssets: async function loadBicycleAssets(id) {
+    if (!id) {
+      throw new Error('Bicycle ID is invalid')
+    }
+
+    const { data } = await axios({
+      url: PUBLIC_URL + `/data/bicycles/${id}.json`
+    })
+
+    const actions = [
+      bicycle.add(data.bicycle),
+      drivetrain.reset(),
+      drivetrain.add(data.drivetrain),
+      paint.reset(),
+      paint.add(data.paint),
+      pedal.reset(),
+      pedal.add(data.pedal),
+      saddle.reset(),
+      saddle.add(data.saddle)
+    ]
+
+    forEach(store.dispatch, actions)
   }
 }
