c0931b69c802afd9e1b02187c07c86b0ad4ae5bd
Illarion Koperski <illarion@outlook.com>
2019-02-09 11:18:21 +0100

    Add commission state to store

--- src/ducks/index.js
+++ src/ducks/index.js
@@ -4,19 +4,22 @@ import { composeWithDevTools } from 'redux-devtools-extension/logOnly'
 import { loggerMiddleware } from './logger'
 
 import { bicycle } from './bicycle'
+import commission from './commission'
 
 const STORE = 'store_9d7eb9682ad411e9917210ddb1eacae1'
 
 const initialState = {
   assets: {
     bicycle: bicycle.initialState
-  }
+  },
+  commission: commission.initialState
 }
 
 const rootReducer = combineReducers({
   assets: combineReducers({
     bicycle: bicycle.reducer
-  })
+  }),
+  commission: commission.reducer
 })
 
 const composeEnhancers = composeWithDevTools({})
